4ba2111184b39cea22e563bc780e7875fd40507d
furkansenyurt <mfurkansenyurt@gmail.com>
2017-02-01 21:33:53 +0300

    pom.xml,App.java,AppTest.java,procfile,compute.mustache all of them update

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,27 +36,31 @@ public class AppTest
     {
         assertTrue( true );
     }
-	 public void testFound() {
+
+    public void testFound() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertTrue(new App().search(array, 4));
+      assertTrue(new App().search(array, 2, 2));
     }
 
     public void testNotFound() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertFalse(new App().search(array, 5));
+      assertFalse(new App().search(array, 10, 2));
     }
 
     public void testEmptyArray() {
       ArrayList<Integer> array = new ArrayList<>();
-      assertFalse(new App().search(array, 1));
+      assertFalse(new App().search(array, 2, 2));
     }
 
     public void testNull() {
-      assertFalse(new App().search(null, 1));
+      assertFalse(new App().search(null, 3, 5));
     }
-
-
-
-
-
+   public void testFirstIntNegativeAndSecontIntEven() {
+     ArrayList<Integer> array = new ArrayList<>(Arrays.asList(4,2,16,8));
+     assertTrue(new App().search(array, -2, 2));
+    }
+   public void testFirstIntNegativeAndSecondintOdd() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(4,2,16,-8));
+      assertTrue(new App().search(array, -2, 3));
+    }
 }
